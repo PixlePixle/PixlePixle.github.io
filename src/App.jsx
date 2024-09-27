1cae87f17c08625dee20acee2b1534240474746d
PixlePixle <67672793+PixlePixle@users.noreply.github.com>
2024-09-27 16:44:55 -0400

    Followed tutorial from YT design.
    https://www.youtube.com/watch?v=xtK57s6QN28

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import './App.css'
+import homeImage from './assets/main.jpg'
 
 function App() {
 
   return (
     <>
-      {/* Background Image Div */}
-      <div id='wallpaper'><div class="blur" /></div>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"></link>
+      <header>
+        <a href='#' className='logo'>Peter</a>
 
-      <div className='header'>
-        <h3 className='beiruti-regular'>about</h3>
-        <h1 className='beiruti-regular'>peter jung</h1>
-        <h3 className='beiruti-regular'>projects</h3>
+        <nav>
+          <a href='#' className='active'>Home</a>
+          <a href='#'>Experience</a>
+          <a href='#'>Projects</a>
+          <a href='#'>Contact</a>
+        </nav>
+      </header>
+
+      <section className='home'>
+        {/* <div className='home-img'>
+          <img src={homeImage} alt='' />
+        </div> */}
+        <div className='home-content'>
+          <h1>Hi, it's <span>Peter</span></h1>
+          <h3 className='typing-text'>I'm a <span></span></h3>
+          <p>Irure anim elit occaecat non in aliqua proident cillum veniam culpa nulla.</p>
+          <div className="social-icons">
+          <a href="#"><i className='fa-brands fa-linkedin'></i></a>
+          <a href="#"><i className='fa-brands fa-github'></i></a>
+          </div>
+          <a href="#" className='btn'>Hire me</a>
         </div>
-      <div id='content'>
-        <p className='beiruti-regular' >a student trying to leave his mark on the world</p>
-      </div>
-
+      </section>
     </>
   )
 }
